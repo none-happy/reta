7ad4fbce4b6f2f606a5daf1ee75318238566e5a8
none-happy <xn4518179591@126.com>
2022-09-27 16:57:54 +0800

    output print

--- gfx/drivers/gl2.c
+++ gfx/drivers/gl2.c
@@ -3696,6 +3696,7 @@ static void *gl2_init(const video_info_t *video,
    }
    //char ch[128];
    //sprintf(ch,"%d",)
+   printf("hahaahahahhahahaah%s\n", gl->ctx_driver->ident);
    if (     !gl->ctx_driver->set_video_mode
          || !gl->ctx_driver->set_video_mode(gl->ctx_data,
             win_width, win_height, video->fullscreen))
